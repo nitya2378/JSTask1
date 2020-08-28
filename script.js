95ca37d50a21761e87485ec56de2ffc30911902b
nitya2378 <69053344+nitya2378@users.noreply.github.com>
2020-08-28 10:11:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var nameElem = document.getElementById('name');
+console.log(nameElem);
+var emailElem = document.getElementById('email');
+console.log(emailElem);
+var contactElem = document.getElementById('contact');
+console.log(contactElem);
+
+// var myForm = document.getElementById('my-form');
+// myForm.addEventListener('submit', function(e){
+//     e.preventDefault();
+//     console.log("submitted");
+// });
